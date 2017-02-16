4e0cae2fb08c6b30d552f344b69fc8ef62f3796b
alex339 <anna-muljar@rambler.ru>
2017-02-16 14:01:03 +0300

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 
 gulp.task("less", function () {
-    gulp.src('less/myStyles.less')
-    .pipe(less())   .pipe(gulp.dest('less/myStyles.less'));
+   gulp.src('less/myStyles.less')
+     .pipe(less())   .pipe(gulp.dest('less/myStyles.css'));
 })
