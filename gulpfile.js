2e344332ac32dc0532f0c258c7f60cf2ae0d8f93
alex339 <anna-muljar@rambler.ru>
2017-02-17 11:27:20 +0300

    modified

--- gulpfile.js
+++ gulpfile.js
@@ -4,4 +4,8 @@ var less = require('gulp-less');
 gulp.task("less", function () {
    gulp.src('less/myStyles.less')
      .pipe(less())   .pipe(gulp.dest('less/myStyles.css'));
-})
+});
+
+gulp.task('less:watch', function(){
+    gulp.watch('less/myStyles.css', ['less']);
+});
