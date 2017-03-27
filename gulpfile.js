5d1621f95a517abbb50564da0a9e266b9ae2e33f
alex339 <anna-muljar@rambler.ru>
2017-03-27 12:07:39 +0300

    gulp less added and page with quiz results started

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,8 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
-var browserSync = require('browser-sync').create();
 
 gulp.task("less", function () {
    gulp.src('less/mystyles.less')
      .pipe(less())   .pipe(gulp.dest('css'));
 });
 
-gulp.task('dev', ['browserSync', 'less'], function() {
-    gulp.watch('less/*.less', ['less']);
-    // Reloads the browser whenever HTML or JS files change
-    gulp.watch('*.html', browserSync.reload);
-    gulp.watch('js/**/*.js', browserSync.reload);
-    gulp.watch('css/**/*.css', browserSync.reload);
-});
-
-// Configure the browserSync task
-gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-});
